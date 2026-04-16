ff47e48d3e810a006dffb71919ab47a1d08d6168
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-04-16 17:58:43 -0600

    Add files via upload

---
+++ C#/Act2_Lecc7_Inciso2.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la calificación (0-100):");
+        double nota = Convert.ToDouble(Console.ReadLine());
+        string msj = "";
+
+        if (nota >= 95) msj = "Rango: EXCELENTE";
+        else if (nota >= 85) msj = "Rango: MUY BIEN";
+        else if (nota >= 75) msj = "Rango: BIEN";
+        else if (nota >= 60) msj = "Rango: REGULAR";
+        else msj = "Rango: DEFICIENTE";
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine(msj);
+    }
+}
