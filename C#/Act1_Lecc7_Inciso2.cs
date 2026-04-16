f5540f144ff45acf6e41755bebb43b2cde63dcf7
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-04-16 17:52:58 -0600

    Update Act1_Lecc7_Inciso2.cs

--- C#/Act1_Lecc7_Inciso2.cs
+++ C#/Act1_Lecc7_Inciso2.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese su edad:");
+        double edad = Convert.ToDouble(Console.ReadLine());
+        string msj = "";
+
+        if (edad >= 18)
+        {
+            msj = "ESTADO: Mayor de edad";
+        }
+        else
+        {
+            msj = "ESTADO: Menor de edad";
+        }
+
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine(msj);
+    }
+}
