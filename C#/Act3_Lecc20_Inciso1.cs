d379cfcf02b959f0b1ac77b2aa0812d9eb175053
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-04-16 18:31:20 -0600

    Add files via upload

---
+++ C#/Act3_Lecc20_Inciso1.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingrese día (1-7): ");
+        int dia = Convert.ToInt32(Console.ReadLine());
+        string msj = "";
+
+        switch (dia)
+        {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                msj = "ENTRE SEMANA"; break;
+            case 6:
+            case 7:
+                msj = "FIN DE SEMANA"; break;
+            default:
+                msj = "DÍA NO EXISTE"; break;
+        }
+    }
+}
