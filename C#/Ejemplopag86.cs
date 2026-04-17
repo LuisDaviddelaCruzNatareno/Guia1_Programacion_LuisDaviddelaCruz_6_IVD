6933d1861030a79d69590a2afd5f41bc1d7af3f6
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-04-16 18:13:36 -0600

    Add files via upload

---
+++ C#/Ejemplopag86.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 0;
+        Console.WriteLine("INtroduzca un número:");
+        num = int.Parse(Console.ReadLine());
+        if(num<0)
+        {
+            Console.WriteLine("El número es menor a cero");
+        }
+        else
+        {
+            Console.WriteLine("El número es igual o menor a cero");
+        }
+    }
+}
