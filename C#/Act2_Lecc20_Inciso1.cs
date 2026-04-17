5202cabe84988e5b043522aae4266a95dd3a44bd
LuisDaviddelaCruzNatareno <ldelacruzdb23@gmail.com>
2026-04-16 18:29:11 -0600

    Add files via upload

---
+++ C#/Act2_Lecc20_Inciso1.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ingrese Contraseña: ");
+        string clave = Console.ReadLine();
+        string msj = "";
+
+        if (clave == "Password123")
+        {
+            msj = "¡BIENVENIDA!";
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        else
+        {
+            msj = "INGRESO PROHIBIDO";
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+
+        Console.WriteLine("\n" + msj);
+        Console.ResetColor();
+    }
+}
